1245996c7cf24bd893eb7eeacb46eb6cde2d91bc
faissal20 <fayssalz15z@gmail.com>
2024-02-21 00:49:39 +0100

    add comment for optimization

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,21 @@ Route::get('/dashboard', function () {
 Route::get('/home',[HomeController::class,'index'])->middleware('auth')->name('home');
 
 Route::middleware('auth')->group(function () {
+
+    // since you have two type of users you should use routes for /admin and / for users
+
+    // create Folder on Controllers called Admin and create a new HomeController on it there you should put the admin dashboard and needed functions
+    // and leave a HomeController for users that redirect to the user dashboard
+
+    // had route hna 3adl fih dik xi d admin kaml 
+    // Route::prefex('admin')->group(function () {
+    //     Route::get('/dashboard', function () {
+    //         return view('admin.dashboard');
+    //     })->middleware(['auth', 'verified'])->name('admin.dashboard');
+    // });
+
+
+    // o hada khalih l users 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
